821dc2bf6f21484861edf15c0d912f3e4a59119d
sae1013 <sae1013@gmail.com>
2022-08-18 19:46:49 +0900

    웹팩 config파일 생성

--- public/index_bundle.js/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log("임포트 파일입니다."),document.querySelector("#root").innerHTML="HelloWorld"})();
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+  mode:'development', //! 다음에는 런타임 환경을 읽어서 prod/dev 설정하도록 변경할것
+  entry: "./source/index.js",
+  output: {
+    path : path.resolve(__dirname,"dist"),
+    filename:'[name]_bundle.js'
+  }
+}
